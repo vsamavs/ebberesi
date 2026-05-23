1057cdbc9f7897f108f0249423703468347ec824
Vincenzo Samà <v.sama@reply.it>
2026-05-23 15:32:00 +0200

    temp log

--- api/satispay-create-payment.js
+++ api/satispay-create-payment.js
@@ -58,7 +58,7 @@ export default async function handler(req, res) {
       satispayPaymentId: payment.id,
       status: 'pending_payment',
     });
-
+console.log('Satispay payment object:', JSON.stringify(payment, null, 2));
     res.status(200).json({
       paymentId: payment.id,
       redirectUrl,
